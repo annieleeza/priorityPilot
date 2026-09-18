83f8d77bb23613310e60f166baff50229ebf140e
annieleeza <annieleeza.arockiaraj@gmail.com>
2026-09-18 20:59:34 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from app.priority.priority_gen import create_task
+from app.priority.priority_decision import (
+    calculate_priority,
+    get_priority_level
+)
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route("/test", methods=["POST"])
+    def test_priority():
+        data = request.json
+
+        task = create_task(
+            data["name"],
+            data["deadline"],
+            data["estimated_hours"],
+            data["importance"],
+            data["difficulty"]
+        )
+
+        score = calculate_priority(task)
+        level = get_priority_level(score)
+
+        return jsonify({
+            "task": task,
+            "score": score,
+            "priority": level
+        })
 
     return app
