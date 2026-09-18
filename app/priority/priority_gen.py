58b15f3ad1272670340948731c27c23421d85f2a
annieleeza <annieleeza.arockiaraj@gmail.com>
2026-09-18 20:46:14 +0530

    Add create_task function to generate task dictionary

--- app/priority/priority_gen.py
+++ app/priority/priority_gen.py
@@ -1,1 +1,10 @@
+def create_task(name, deadline, estimated_hours, importance, difficulty):
+    task = {
+        "name": name,
+        "deadline": deadline,
+        "estimated_hours": estimated_hours,
+        "importance": importance,
+        "difficulty": difficulty
+    }
 
+    return task
